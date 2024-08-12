99c2947ad3519d838ad1546940f4a798c7a7a249
人間工作 <coolxiaom95@gmail.com>
2024-08-12 22:17:46 +0800

    gradle-ify

---
+++ build.gradle.kts
@@ -0,0 +1,66 @@
+plugins {
+    java
+    `maven-publish`
+    id ("com.github.johnrengelman.shadow") version "7.0.0"
+}
+
+group = "ml.mcos"
+version = "1.3.1"
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+    maven("https://repo.codemc.io/repository/maven-public/")
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+}
+
+dependencies {
+    compileOnly("org.spigotmc:spigot-api:1.20-R0.1-SNAPSHOT")
+    compileOnly("net.milkbowl.vault:VaultAPI:1.7")
+
+    implementation("org.jetbrains:annotations:21.0.0")
+}
+val targetJavaVersion = 8
+java {
+    val javaVersion = JavaVersion.toVersion(targetJavaVersion)
+    if (JavaVersion.current() < javaVersion) {
+        toolchain.languageVersion.set(JavaLanguageVersion.of(targetJavaVersion))
+    }
+}
+tasks {
+    shadowJar {
+        archiveClassifier.set("")
+        mapOf(
+            "org.intellij.lang.annotations" to "annotations.intellij",
+            "org.jetbrains.annotations" to "annotations.jetbrains",
+        ).forEach { (original, target) ->
+            relocate(original, "ml.mcos.litelottery.libs.$target")
+        }
+    }
+    build {
+        dependsOn(shadowJar)
+    }
+    withType<JavaCompile>().configureEach {
+        options.encoding = "UTF-8"
+        if (targetJavaVersion >= 10 || JavaVersion.current().isJava10Compatible) {
+            options.release.set(targetJavaVersion)
+        }
+    }
+    processResources {
+        duplicatesStrategy = DuplicatesStrategy.INCLUDE
+        from(sourceSets.main.get().resources.srcDirs) {
+            expand(mapOf("version" to version))
+            include("plugin.yml")
+        }
+    }
+}
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components.getByName("java"))
+            groupId = project.group.toString()
+            artifactId = rootProject.name
+            version = project.version.toString()
+        }
+    }
+}
